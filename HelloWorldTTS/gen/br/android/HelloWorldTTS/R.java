fc5de9cb52ce865c7e20eed64f3b76d94b1da40a
GlauberCamel <Heero_Argentino@Hotmail.com>
2011-10-21 12:16:44 -0300

    Acrescentado um visual novo e opções para escolher o idioma no qual será utilizado o TTS.

--- HelloWorldTTS/gen/br/android/HelloWorldTTS/R.java
+++ HelloWorldTTS/gen/br/android/HelloWorldTTS/R.java
@@ -12,6 +12,18 @@ public final class R {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
+        public static final int labi9=0x7f020001;
+    }
+    public static final class id {
+        public static final int button1=0x7f050003;
+        public static final int button2=0x7f050004;
+        public static final int button3=0x7f050005;
+        public static final int button4=0x7f050006;
+        public static final int button5=0x7f050008;
+        public static final int linearLayout1=0x7f050002;
+        public static final int linearLayout2=0x7f050007;
+        public static final int linearLayout4=0x7f050000;
+        public static final int textView1=0x7f050001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- HelloWorldTTS/src/br/android/HelloWorldTTS/HelloWorldTTSActivity.java
+++ HelloWorldTTS/src/br/android/HelloWorldTTS/HelloWorldTTSActivity.java
@@ -3,13 +3,15 @@ package br.android.HelloWorldTTS;
 import java.util.Locale;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.speech.tts.TextToSpeech.OnInitListener;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 /**
- * TTS (Text-to-Speech) do GPS android. Implementa a OnInitListener
+ * TTS (Text-to-Speech) do GPS android
  * @author Glauber
  * 
  */
@@ -19,34 +21,83 @@ public class HelloWorldTTSActivity extends Activity implements OnInitListener {
 	private TextToSpeech tts;
 
 	
-    /** Called when the activity is first created. */
+    /** 
+     * Apresenta o layout e , de acordo com a opção escolhida, seleciona o idioma.
+     * @param savedInstanceState
+     */
 
     public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-
-        Intent checkIntent = new Intent();
-        checkIntent.setAction(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
-        startActivityForResult(checkIntent, 0);
+        
+        Button buttonUK = (Button) findViewById(R.id.button1);
+        Button buttonUS = (Button) findViewById(R.id.button2);
+        Button buttonFrances = (Button) findViewById(R.id.button3);
+        Button buttonAlemao = (Button) findViewById(R.id.button4);
+        Button buttonTestar = (Button) findViewById(R.id.button5);
+        
         tts = new TextToSpeech(this, this);
-
+        
+        buttonUK.setOnClickListener(new OnClickListener() { 
+			
+			public void onClick(View v){ 
+				tts.setLanguage(Locale.UK);
+			}
+		});
+        
+        buttonUS.setOnClickListener(new OnClickListener() { 
+			
+			public void onClick(View v){ 
+				tts.setLanguage(Locale.US);
+			}
+		});
+        
+        buttonFrances.setOnClickListener(new OnClickListener() { 
+			
+			public void onClick(View v){ 
+				tts.setLanguage(Locale.FRENCH);
+			}
+		});
+        
+        buttonAlemao.setOnClickListener(new OnClickListener() { 
+			
+			public void onClick(View v){ 	   
+				tts.setLanguage(Locale.GERMAN);
+				}
+		});
+        
+        buttonTestar.setOnClickListener(new OnClickListener() { 
+			
+			public void onClick(View v){ 	   
+				Speach();
+				}
+		});
     }
-
+    
     
     /**
      * Função responsável por pegar as strings e converter para fala
      * @param arg0
      */
-    public void onInit(int arg0) {
+    public void Speach() {
+    	
     	//Strings que serão faladas ao iniciar o programa
 	    String speech1 = "Google maps";
 	    String speech2 = "Hello World, people";
 	    //Define qual a linguagem que será utilizada para falar a frase contida na string
-	    tts.setLanguage(Locale.US);
 	    //Chamada da função de conversão da string para a fala
 	    tts.speak(speech1, TextToSpeech.QUEUE_FLUSH, null);
 	    tts.speak(speech2, TextToSpeech.QUEUE_ADD, null);
     }
+   
+	/**
+	 * Função executada ao iniciar o app
+	 * @param status
+	 */
+	public void onInit(int status) {
+		String speech1 = "Hello, buddy!! Choose you idioma. Thanks!!";
+		tts.speak(speech1, TextToSpeech.QUEUE_FLUSH, null);
+	}
 
 }
